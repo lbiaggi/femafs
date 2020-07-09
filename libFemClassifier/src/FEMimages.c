9c3ac4b40420dfce4177927777efe7b308d4bec3
Tomaz Canabrava <tcanabrava@kde.org>
2020-07-09 17:43:40 +0200

    Move headers and clear whitespaces

--- libFemClassifier/src/FEMimages.c
+++ libFemClassifier/src/FEMimages.c
@@ -4,20 +4,20 @@ void printPoints(char name_img[], FEMDataset *dataset, int nx, int ny)
 {
   FILE *img = fopen(name_img,"w");
   fprintf(img,"P3\n");
-  fprintf(img,"%d %d 255\n",nx,ny);  
-  
+  fprintf(img,"%d %d 255\n",nx,ny);
+
   int i,j,k;
   int count = 0;
-  
+
   for(i = 0; i < nx; i++)
   {
-    
+
     for(j = 0; j < ny; j++)
-    {      
+    {
         int print = 0;
 	for(k = 0; k < dataset->number_of_samples; k++)
 	{
-	  if(i == dataset->samples[k].features[0] && j == dataset->samples[k].features[1] || 
+	  if(i == dataset->samples[k].features[0] && j == dataset->samples[k].features[1] ||
 	    i == dataset->samples[k].features[0]-1 && j == dataset->samples[k].features[1] ||
 	    i == dataset->samples[k].features[0]+1 && j == dataset->samples[k].features[1] ||
 	    i == dataset->samples[k].features[0]   && j == dataset->samples[k].features[1]-1 ||
@@ -25,10 +25,10 @@ void printPoints(char name_img[], FEMDataset *dataset, int nx, int ny)
 	    i == dataset->samples[k].features[0]-1 && j == dataset->samples[k].features[1]-1 ||
 	    i == dataset->samples[k].features[0]-1 && j == dataset->samples[k].features[1]+1 ||
 	    i == dataset->samples[k].features[0]+1 && j == dataset->samples[k].features[1]-1 ||
-	    i == dataset->samples[k].features[0]+1 && j == dataset->samples[k].features[1]+1)	  
+	    i == dataset->samples[k].features[0]+1 && j == dataset->samples[k].features[1]+1)
 	    print = dataset->samples[k].class;
 	}
-	
+
 	switch(print)
 	{
 	  case 0: fprintf(img,"255 255 255 "); break;
@@ -43,15 +43,15 @@ void printPoints(char name_img[], FEMDataset *dataset, int nx, int ny)
 	else
 	  fprintf(img," %d %d %d ",(int)print*(255/9),(int)print*(255/9),(int)print*(255/9));
 	*/
-	
+
 	count++;
 	if(count == 8)
 	{
 	  fprintf(img,"\n");
 	  count = 0;
-	}      
+	}
     }
-  }  
+  }
   fclose(img);
-}  
+}
 
