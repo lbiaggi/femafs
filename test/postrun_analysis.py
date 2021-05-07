c3d9bd5d7783df6b9561e21dec1174c9d8021f20
Lucas Biaggi <lbjanuario@gmail.com>
2021-05-07 00:30:55 -0300

    insert confusion matrix

--- test/postrun_analysis.py
+++ test/postrun_analysis.py
@@ -13,6 +13,43 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import matthews_corrcoef
 from sklearn.metrics import average_precision_score
 
+def generate_cm(fem:str, data:list, fix:str=None):
+    if fix:
+        files = sorted([ name for name in os.listdir(os.getcwd()) if
+            os.path.isfile(os.path.join(os.getcwd(), name))
+            and (fem in name and name.startswith(fix))])
+    else:
+        files = sorted([ name for name in os.listdir(os.getcwd()) if
+            os.path.isfile(os.path.join(os.getcwd(), name)) and fem in name])
+
+    for f in files:
+        df = pd.read_csv(f, sep=" ", header=None)
+        line = f.replace("-", "").split("_")
+        if len(line) > 5:
+            method = line[0]
+            iteration = line[2].replace("-", "")
+            dataset = line[3]
+            percentage = line[4][:-1]
+
+        else:
+            method = line[0]
+            iteration = line[1]
+            dataset = line[2].replace("-", "")
+            percentage = line[3][:-1]
+
+        dline = {"method" : method,
+                 "fem":fem,
+                 "iteration" : iteration,
+                 "dataset" : dataset,
+                 "percentage": percentage,
+                 }
+        # tn, fp, fn, tp = confusion_matrix(df[0], df[1]).ravel()
+        dline["true_negative"], dline["false_positive"],\
+        dline["false_negative"], dline["true_positive"] = confusion_matrix(df[0],df[1]).ravel()
+        # if dataset == 'tornontor':
+             # print(dline)
+        data.append(dline)
+
 
 def generate_dict_data(fem:str, data:list, fix:str=None):
     if fix:
@@ -56,7 +93,7 @@ def generate_dict_data(fem:str, data:list, fix:str=None):
 
 
 
-def walk_through_testdir(directory:str, finaldata:list):
+def walk_through_testdir(directory:str, finaldata:list, cmdata:list = None):
     os.chdir(directory)
     print(os.getcwd())
     dset, perc, fem = directory.split('_')[1:]
@@ -65,16 +102,26 @@ def walk_through_testdir(directory:str, finaldata:list):
         generate_opf_pd(dset,perc,fem)
         generate_dict_data(fem, finaldata, 'fema')
         generate_dict_data(fem, finaldata, 'opf')
+        if cmdata:
+            generate_cm(fem, cmdata, 'fema')
+            generate_cm(fem, cmdata, 'opf')
     elif "unswnb15" == dset.lower():
         generate_fema_opf_pd(dset, perc,fem)
         generate_opf_pd(dset,perc,fem,'unsw-nb15')
         generate_dict_data(fem, finaldata, 'fema')
         generate_dict_data(fem, finaldata, 'opf')
+        if cmdata:
+            generate_cm(fem, cmdata, 'fema')
+            generate_cm(fem, cmdata, 'opf')
 
     else:
         generate_fema_opf_pd(dset, perc,fem)
         generate_opf_pd(dset,perc,fem)
         generate_dict_data(fem, finaldata)
+        if cmdata:
+            generate_cm(fem, cmdata, 'fema')
+            generate_cm(fem, cmdata, 'opf')
+
 
     os.chdir("..")
 
@@ -100,8 +147,6 @@ def generate_fema_opf_pd(dataset:str, percentage:str, fem:str,
         except Exception as e:
            print(f"Error processing data from {d}, {dataset}, {percentage},{fem}")
            print(e)
-        except:
-           print(f"Error processing data from {d}, {dataset}, {percentage},{fem}")
 
 
 def generate_opf_pd(dataset:str, percentage:str, fem:str,
@@ -127,8 +172,6 @@ def generate_opf_pd(dataset:str, percentage:str, fem:str,
         except Exception as e:
            print(f"Error processing data from {d}, {dataset}, {percentage},{fem}")
            print(e)
-        except:
-           print(f"Error processing data from {d}, {dataset}, {percentage},{fem}")
 
 
 
@@ -137,17 +180,23 @@ def generate_opf_pd(dataset:str, percentage:str, fem:str,
 def walk_through_datasets(startdir:str, dataset:list):
     os.chdir(startdir)
     pandaslist = []
+    cmpandaslist = []
     for d in dataset:
         print(f"Walking through dataset {d} tests")
         dirs = sorted([ name for name in os.listdir(startdir) if
             os.path.isdir(os.path.join(startdir, name)) and d in name])
         for i in dirs:
-            walk_through_testdir(i, finaldata=pandaslist)
+            walk_through_testdir(i, finaldata=pandaslist, cmdata = cmpandaslist)
         print(f"Done with dataset")
     df = pd.DataFrame(data=pandaslist)
     df['dataset'] = df['dataset'].apply(lambda x: x.lower().replace('-', ''))
     print(f"Generating final_data_{dataset[0]}.csv in {startdir}")
     df.to_csv(f"${CMAKE_CURRENT_BINARY_DIR}/final_data_{dataset[0].lower()}.csv", index=False)
+    print(f"File Generated")
+    cm = pd.DataFrame(data=cmpandaslist)
+    cm['dataset'] = cm['dataset'].apply(lambda x: x.lower().replace('-', ''))
+    print(f"Generating final_data_{dataset[0]}_cm.csv in {startdir}")
+    cm.to_csv(f"${CMAKE_CURRENT_BINARY_DIR}/final_data_{dataset[0].lower()}_cm.csv", index=False)
     print(f"File Generated")
 
 if __name__ == '__main__':
